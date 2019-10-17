5e255310f056b47fb2643d77a89064b791963174
naimahassan <hassannaima77@gmail.com>
2019-10-17 18:06:39 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Content from './components/Content'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Router>
+          <Sidebar />
+          <Route path='/' Component={Content} />
+        </Router>
+      </div>
+    )
+  }
+}
+
+export default App
